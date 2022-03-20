a2de181e420c98bf5e65a6208c9196a151e51f72
Hanyang Xiao <xhanyang0901@gmail.com>
2022-03-20 16:45:04 -0400

    Simple UI

---
+++ app/src/main/java/edu/neu/cs5520/alphaobserver/companyInfo/InfoDescriptionActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.cs5520.alphaobserver.companyInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import edu.neu.cs5520.alphaobserver.R;
+
+import android.os.Bundle;
+
+public class InfoDescriptionActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_description);
+    }
+}
---
+++ app/src/main/java/edu/neu/cs5520/alphaobserver/companyInfo/companyInfoActivity.java
@@ -0,0 +1,17 @@
+package edu.neu.cs5520.alphaobserver.companyInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.neu.cs5520.alphaobserver.R;
+
+public class companyInfoActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_info);
+    }
+}
