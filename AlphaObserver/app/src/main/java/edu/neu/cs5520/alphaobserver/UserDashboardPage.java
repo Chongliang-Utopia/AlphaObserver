cea717b7411ef342d23bc54e88bb05f103e662ee
LINNI2017 <cai20170607@gmail.com>
2022-03-21 16:46:59 -0700

    add user dashboard

---
+++ AlphaObserver/app/src/main/java/edu/neu/cs5520/alphaobserver/UserDashboardPage.java
@@ -0,0 +1,14 @@
+package edu.neu.cs5520.alphaobserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDashboardPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard_page);
+    }
+}
