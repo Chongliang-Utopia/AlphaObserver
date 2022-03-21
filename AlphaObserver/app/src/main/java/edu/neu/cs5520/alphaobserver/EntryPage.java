9225cae9fa3d54433830b37a10c314d4722af953
LINNI2017 <cai20170607@gmail.com>
2022-03-21 14:07:38 -0700

    add login page

--- AlphaObserver/app/src/main/java/edu/neu/cs5520/alphaobserver/EntryPage.java
+++ AlphaObserver/app/src/main/java/edu/neu/cs5520/alphaobserver/EntryPage.java
@@ -2,13 +2,30 @@ package edu.neu.cs5520.alphaobserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EntryPage extends AppCompatActivity {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_page);
+
+        loginButton = (Button) findViewById(R.id.button_entry_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginPage();
+            }
+        });
+    }
+
+    private void openLoginPage() {
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
     }
 }
