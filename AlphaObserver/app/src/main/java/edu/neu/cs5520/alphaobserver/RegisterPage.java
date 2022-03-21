075e566489225346a5c496d11595152292ca72c2
LINNI2017 <cai20170607@gmail.com>
2022-03-21 14:43:05 -0700

    add back button

--- AlphaObserver/app/src/main/java/edu/neu/cs5520/alphaobserver/RegisterPage.java
+++ AlphaObserver/app/src/main/java/edu/neu/cs5520/alphaobserver/RegisterPage.java
@@ -2,13 +2,30 @@ package edu.neu.cs5520.alphaobserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterPage extends AppCompatActivity {
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        backButton = (Button) findViewById(R.id.button_register_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPage(EntryPage.class);
+            }
+        });
+    }
+
+    private void openPage(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
     }
 }
