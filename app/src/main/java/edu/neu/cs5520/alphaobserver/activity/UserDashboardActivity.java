68c8b349fd3d55b74e7d9801e59c643522e409fc
LINNI2017 <cai20170607@gmail.com>
2022-03-29 12:31:45 -0700

    add entry page

---
+++ app/src/main/java/edu/neu/cs5520/alphaobserver/activity/UserDashboardActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.cs5520.alphaobserver.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.neu.cs5520.alphaobserver.R;
+
+public class UserDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard_page);
+    }
+}
